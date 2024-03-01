57314bea79384c8f83440872cc606af87df77a56
tristan <thuckabee.work@gmail.com>
2024-02-29 18:45:50 -0600

    02 done main

--- 02-graph-problems/island.js
+++ 02-graph-problems/island.js
@@ -1,43 +1,34 @@
 function getNeighbors(row, col, graph) {
+  const res = [];
+  
+  if (graph[row - 1] && graph[row - 1][col]) res.push([row - 1, col]);
+  if (graph[row + 1] && graph[row + 1][col]) res.push([row + 1, col]);
+  if (graph[row][col - 1]) res.push([row, col - 1]);
+  if (graph[row][col + 1]) res.push([row, col + 1]);
 
-  // Check top
-
-  // Check bottom
-
-  // Check left
-
-  // Check right
-
-  // Return neighbors
-
-  // Your code here 
+  return res;
 }
 
-
 function islandSize(row, col, graph) {
+  let stack = [[row, col]];
+  let visited = new Set([`${[row, col]}`]);
+  let size = 0;
 
-  // Create a visited set to store visited nodes
-
-  // Create a stack, put the starting node in the stack
-
-  // Put the stringified starting node in visited
-
-  // Initialize size to 0
-
-  // While the stack is not empty,
-
-    // Pop the first node
-
-    // DO THE THING (increment size by 1)
-
-    // Then push all the UNVISITED neighbors on top of the stack
-    // and mark them as visited
-    // HINT: This is what your helper function `getNeighbors` is for
-    // HINT: Remember, you're storing your visited nodes as strings!
-
-  // return size
-
-  // Your code here 
+  while (stack.length) {
+    let curr = stack.pop();
+    
+    size++;
+    
+    let neighbors = getNeighbors(...curr, graph);
+    neighbors.forEach(el => {
+      if (!visited.has(`${el}`)) {
+        visited.add(`${el}`);
+        stack.push(el);
+      }
+    })
+  }
+  
+  return size;
 }
 
 module.exports = [getNeighbors, islandSize];
